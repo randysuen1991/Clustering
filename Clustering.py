55f955131d1982a55e9f2d30eca12f7bc55c8c8d
randysuen1991 <randysuen1991@gmail.com>
2018-10-12 11:23:15 +0800

    new

--- Clustering.py
+++ Clustering.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.cluster import KMeans
+from abc import ABC, abstractmethod
 
 
 class Clustering:
@@ -8,6 +9,8 @@ class Clustering:
 
 
 class Kmeans(Clustering, KMeans):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, n_clusters, init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances='auto',
+                 verbose=0, random_state=None, copy_x=True, n_jobs=None, algorithm='auto'):
+        super().__init__(n_clusters, init, n_init, max_iter, tol, precompute_distances, verbose, random_state, copy_x,
+                         n_jobs, algorithm)
 
---
+++ MixtureModel.py
@@ -0,0 +1,7 @@
+from sklearn.mixture import GaussianMixture as GM
+
+
+class GaussianMixModel(GM):
+    def __init__(self):
+        super().__init__()
+        
