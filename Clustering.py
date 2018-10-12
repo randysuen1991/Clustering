270df5eb7098a3879b341b57ffd0e6e6cb8d60da
randysuen1991 <randysuen1991@gmail.com>
2018-10-12 09:03:57 +0800

    new

--- Clustering.py
+++ Clustering.py
@@ -1,6 +1,13 @@
 import numpy as np
+from sklearn.cluster import KMeans
 
 
 class Clustering:
     def __init__(self):
         pass
+
+
+class Kmeans(Clustering, KMeans):
+    def __init__(self):
+        super().__init__()
+
